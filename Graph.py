8f86110afc53b119a90cea271a72a5a9e5ff199b
morgankrueger <morgankrueger17@gmail.com>
2024-12-02 21:07:17 -0700

    Added sources

--- Graph.py
+++ Graph.py
@@ -8,11 +8,14 @@ csv_files = ["2cars7seats.csv", "4cars7seats.csv", "6cars7seats.csv"]
 
 #plot the waiting queue
 plt.figure(1)  # First figure
+
+##Utilizing ChatGPT for generalized graphing of data for column 3 vs column 1 in 3 csv files##
 for file in csv_files:
     data = pd.read_csv(file)
     x = data.iloc[:, 0]  # Column 1
     y = data.iloc[:, 2]  # Column 3
     plt.plot(x, y, label=file)
+
 
 # Plot all data on the same plot
 plt.xlabel('Time (Minutes)')
@@ -25,11 +28,14 @@ plt.ylim(0,1000)
 
 # Plot the rejected
 plt.figure(2)  # Second figure
+
+##Utilizing ChatGPT for generalized graphing of data for column 4 vs column 1 in 3 csv files##
 for file in csv_files:
     data = pd.read_csv(file)
     x = data.iloc[:, 0]  # Column 1
     y = data.iloc[:, 3]  # Column 4
     plt.plot(x, y, label=file)
+
 
 plt.xlabel('Time (minutes)')
 plt.ylabel('# of persons being rejected')
@@ -37,7 +43,7 @@ plt.title('GoAway People')
 plt.legend()
 plt.grid(True)
 
-# Plot Column 2 vs Column 1 for a specific file
+##Utilizing ChatGPT for generalized graphing of data for column 2 vs column 1 in 1 specific csv file##
 specific_file = "2cars7seats.csv" 
 plt.figure(3)  # Third figure
 data = pd.read_csv(specific_file)
@@ -45,11 +51,10 @@ x = data.iloc[:, 0]  # Column 1
 y = data.iloc[:, 1]  # Column 2
 plt.plot(x, y, "r-D", markersize=4, linewidth='.5',label="25,45,35,25")
 
+
 plt.xlabel('Time (minutes)')
 plt.ylabel('Arrival #')
 plt.title('People Arrival')
 plt.legend()
 plt.grid(linestyle='dashed')
-
-# Show all figures
 plt.show()
